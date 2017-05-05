998c3a0042c97670fbedb70191a3478009b2e344
R. Aguayo <bexterjetiahedgehog@gmail.com>
2017-05-05 18:56:41 -0400

    Add files via upload

---
+++ pp.py
@@ -0,0 +1,15 @@
+
+#this is a program in python that caluclates your korean age
+
+w= input(" year you were born: " )
+#enter birth year
+
+w= float (w)
+h = input ("enter current year : " )
+#enter current year 
+h= float (h)
+# calculate your korean age by subtracting the year you were born from the current year and adding one
+k_age= h - w +1
+print (k_age) 
+
+        
